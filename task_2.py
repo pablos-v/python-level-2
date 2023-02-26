675e440a900c090e97d1899eac04b862622ef45d
Pablos <pablos@list.ru>
2023-02-25 22:45:19 -0300

    task_2 solved

--- task_2.py
+++ task_2.py
@@ -4,3 +4,19 @@
 ✔ Значения не удаляются, а помещаются в одноимённые переменные без s на конце.
 """
 
+
+def replacer() -> dict:
+    new_variables = {}
+    for var in globals():
+        if var.endswith('s'):
+            new_variables[var[:-1]] = globals()[var]
+            globals()[var] = None
+    return new_variables
+
+
+lights = ''
+sport = []
+stops = []
+
+print(replacer())
+print(globals())
