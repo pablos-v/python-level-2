aa4ad1b88b47d8dd77e994d84c2374bf7b7652df
Pablos <pablos@list.ru>
2023-02-26 15:48:31 -0300

    task_4 fixed

--- task_4.py
+++ task_4.py
@@ -8,8 +8,12 @@
 def strange_task(**kwargs) -> dict:
     res = {}
     for v, k in kwargs.items():
+        try:
+            hash(k)
+        except Exception:
+            k = str(k)
         res[k] = v
     return res
 
 
-print(strange_task(a=23, b='qwerty', c=14.2, d=None, www=True))
+print(strange_task(a=23, b='qwerty', c=14.2, d=None, www=True, ls=[1, 7]))
