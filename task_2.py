043091265c2b323a2134f4a0114d3529ee29eeea
Pablos <pablos@list.ru>
2023-03-05 18:02:07 -0300

    task_2, task_3 done

--- task_2.py
+++ task_2.py
@@ -4,4 +4,13 @@
 """
 
 
+def spliitter(s: str) -> tuple[str, str, str]:
+    st = s.split('.')
+    file_ext = st.pop()
+    st = st[0].split('/')
+    file_name = st.pop()
+    file_path = '/'.join(st)
+    return file_path + '/', file_name, file_ext
 
+
+print(spliitter('C:/Cloud/more/file.jpp'))
--- task_3.py
+++ task_3.py
@@ -7,4 +7,12 @@
 """
 
 
+def salaries(names: list[str], salary: list[int], bonus: list[str]) -> dict[str:float]:
+    return {n: t for n, t in zip(names, [s + (s * b / 100) for s, b in zip(salary, [float(i[:-1]) for i in bonus])])}
 
+
+names = ['Vova', 'Alla', 'Nick']
+salary = [10000, 22000, 13000]
+bonus = ['12.32%', '16.08%', '11.33%']
+
+print(salaries(names, salary, bonus))
