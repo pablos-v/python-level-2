84e416464e76aca7da168c0349d281de3ecb96e3
Pablos <pablos@list.ru>
2023-05-08 21:24:19 -0300

    task_4,5,6 done homework poka ne delayu

--- task_5.py
+++ task_5.py
@@ -3,3 +3,41 @@
 """
 
 
+class Rectangle:
+    __slots__ = ('_length', '__width')
+
+    def __init__(self, length, width=None):
+        self._length = length
+        self.__width = width
+
+    def square(self):
+        return self._length ** 2 if self.__width is None else self._length * self.__width
+
+    def perimeter(self):
+        return self._length * 4 if self.__width is None else (self._length + self.__width) * 2
+
+    @property
+    def length(self):
+        return f'length = {self._length}'
+
+    @length.setter
+    def length(self, length):
+        self._length = abs(length)
+
+    @property
+    def width(self):
+        return f'length = {self.__width}'
+
+    @width.setter
+    def width(self, width):
+        self.__width = abs(width)
+
+
+if __name__ == '__main__':
+    rect = Rectangle(4, 2)
+    print(f'{rect.square() = }, {rect.perimeter() = }')
+    print(rect.length, rect.width)
+    rect.length = 5
+    rect.width = -5
+    print(f'{rect.square() = }, {rect.perimeter() = }')
+    print(rect.__slots__)
