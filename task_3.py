d053d2192147e54ae8387d3261f1ca5cee9f9fb2
Pablos <pablos@list.ru>
2023-05-12 19:48:01 -0300

    task_3 and 4 ready

--- task_3.py
+++ task_3.py
@@ -2,5 +2,16 @@
 Создайте класс с базовым исключением и дочерние классыисключения:
 ○ ошибка уровня,
 ○ ошибка доступа.
-
 """
+
+
+class MainException(Exception):
+    pass
+
+
+class LevelException(MainException):
+    pass
+
+
+class AccessException(MainException):
+    pass
--- task_4.py
+++ task_4.py
@@ -8,5 +8,36 @@
 Отдельно напишите функцию, которая считывает информацию
 из JSON файла и формирует множество пользователей.
 """
+import os.path
+from pathlib import Path
+import json
 
 
+class User:
+    name = ''
+    a_id = None
+    level = None
+
+    def __init__(self, name, a_id, lvl):
+        self.name = name
+        self.a_id = a_id
+        self.level = lvl
+
+    def __str__(self):
+        return f'level = {self.level}, name = {self.name}, ID = {self.a_id}'
+
+
+def read_and_form(file: Path) -> set[User]:
+    set_of_users = set()
+    with open(file, encoding='utf-8') as file:
+        dct = json.load(file)
+    for level, persons in dct.items():
+        for a_id, name in persons.items():
+            set_of_users.add(User(name, a_id, level))
+    return set_of_users
+
+
+if __name__ == '__main__':
+    ss = read_and_form(Path('task_2.json'))
+    for i in ss:
+        print(i)
