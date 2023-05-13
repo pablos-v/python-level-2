ca56747d5a07c8e326e53b46b8629bbc8a7909ff
Pablos <pablos@list.ru>
2023-05-13 19:45:07 -0300

    task_3 ready

--- task_3.py
+++ task_3.py
@@ -1,13 +1,34 @@
 """
-Напишите для задачи 1 тесты unittest. Проверьте
-следующие варианты:
-возврат строки без изменений
-возврат строки с преобразованием регистра без потери
-символов
-возврат строки с удалением знаков пунктуации
-возврат строки с удалением букв других алфавитов
-возврат строки с учётом всех вышеперечисленных пунктов
-(кроме п. 1)
+Напишите для задачи 1 тесты unittest. Проверьте следующие варианты:
+1 возврат строки без изменений
+2 возврат строки с преобразованием регистра без потери символов
+3 возврат строки с удалением знаков пунктуации
+4 возврат строки с удалением букв других алфавитов
+5 возврат строки с учётом всех вышеперечисленных пунктов (кроме п. 1)
 
 """
 
+import unittest
+
+from task_1 import task_func
+
+
+class TestTaskFunc(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(task_func('qwerty qwerty asd'), 'qwerty qwerty asd', msg='Failed test!')
+
+    def test_2(self):
+        self.assertEqual(task_func('qwerty qwERTY ASD'), 'qwerty qwerty asd', msg='Failed test!')
+
+    def test_3(self):
+        self.assertEqual(task_func('qwerty qw,..erty asd'), 'qwerty qwerty asd', msg='Failed test!')
+
+    def test_4(self):
+        self.assertEqual(task_func('qwйййerty qwысмкerty asd'), 'qwerty qwerty asd', msg='Failed test!')
+
+    def test_5(self):
+        self.assertEqual(task_func('qweваКПrty qWУУЦК-4466ERTy aSd'), 'qwerty qwerty asd', msg='Failed test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
