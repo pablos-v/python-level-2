2205715df4457e8b291ff52399e9063ba57b86f2
Pablos <pablos@list.ru>
2023-05-13 21:57:40 -0300

    task_4 ready

--- task_3.py
+++ task_3.py
@@ -31,4 +31,4 @@ class TestTaskFunc(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
--- task_4.py
+++ task_4.py
@@ -1,12 +1,54 @@
 """
-Напишите для задачи 1 тесты pytest. Проверьте следующие
-варианты:
+Напишите для задачи 1 тесты pytest. Проверьте следующие варианты:
 возврат строки без изменений
-возврат строки с преобразованием регистра без потери
-символов
+возврат строки с преобразованием регистра без потери символов
 возврат строки с удалением знаков пунктуации
 возврат строки с удалением букв других алфавитов
-возврат строки с учётом всех вышеперечисленных пунктов
-(кроме п. 1)
+возврат строки с учётом всех вышеперечисленных пунктов (кроме п. 1)
 """
 
+from string import ascii_letters
+import pytest
+
+
+def task_func(s: str) -> str:
+    """
+    Deleting from string s all symbols, witch are not space or a symbol of the latin alphabet.
+    Returns clear string.
+    >>> task_func('qwerty qwerty asd')
+    'qwerty qwerty asd'
+    >>> task_func('qwerty qwERTY ASD')
+    'qwerty qwerty asd'
+    >>> task_func('qwerty qw,..erty asd')
+    'qwerty qwerty asd'
+    >>> task_func('qwйййerty qwысмкerty asd')
+    'qwerty qwerty asd'
+    >>> task_func('qweваКПrty qWУУЦК-4466ERTy aSd')
+    'qwerty qwerty asd'
+    """
+    res = ''.join(c for c in s if c in ascii_letters or c.isspace())
+    return res.lower()
+
+
+def test_no_changes():
+    assert task_func('qwerty qwerty asd') == 'qwerty qwerty asd'
+
+
+def test_register():
+    assert task_func('qwerty qwERTY ASD') == 'qwerty qwerty asd'
+
+
+def test_punctuation():
+    assert task_func('qwerty qw,..erty asd') == 'qwerty qwerty asd'
+
+
+def test_other_letters():
+    assert task_func('qwйййerty qwысмкerty asd') == 'qwerty qwerty asd'
+
+
+def test_every_issue():
+    assert task_func('qweваКПrty qWУУЦК-4466ERTy aSd') == 'qwerty qwerty asd'
+
+
+if __name__ == '__main__':
+    pytest.main(['-v'])
