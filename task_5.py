5a1432ae9e38051638758cb7fccbc26be6a732ef
Pablos <pablos@list.ru>
2023-05-15 18:02:16 -0300

    tasks_4, 5 ready

--- task_5.py
+++ task_5.py
@@ -10,3 +10,25 @@
 
 """
 
+import argparse
+from datetime import datetime
+from task_4 import convert_str_to_date, DAY, MONTH
+
+
+def parser_func():
+    parser = argparse.ArgumentParser(description='Getting datetime from string')
+    parser.add_argument('--count', default='1')
+    parser.add_argument('--day', default=datetime.now().weekday)
+    parser.add_argument('--month', default=datetime.now().month)
+    args = parser.parse_args()
+    day = DAY[args.day] if isinstance(args.day, int) else args.day
+    month = MONTH[args.month] if isinstance(args.month, int) else args.month
+    return convert_str_to_date(f'{args.count} {day} {month}')
+
+
+if __name__ == '__main__':
+    print(parser_func())
+
+# вызов в терминале:
+# python .\task_5.py --count 4 --day пятн --month янв
+# python .\task_5.py --count 4 --day пятн
